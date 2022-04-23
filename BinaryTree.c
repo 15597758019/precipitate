1b08a6497c730cfc32e4d360fa0cdd595ec54662
luoteng <83630765+15597758019@users.noreply.github.com>
2022-04-23 22:35:16 +0800

    二叉树计算节点数的三种方法
    二叉树计算节点数的三种方法

--- BinaryTree.c
+++ BinaryTree.c
@@ -1,6 +1,6 @@
+#define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
 #include<malloc.h>
-#include<stdlib.h>
 typedef char BTDataType;
 typedef struct BinaryTreeNode
 {
@@ -9,42 +9,53 @@ typedef struct BinaryTreeNode
 	BTDataType data;
 }BTNode;
 
-//鍓嶅簭
-void PreOrder(BTNode* root)
+//前序
+//void PrevOrder(BTNode* root)
+//{
+//	if (root == NULL)
+//	{
+//		printf("NULL ");
+//		return;
+//	}
+//		
+//	printf("%c ", root->data);
+//	PrevOrder(root->left);
+//	PrevOrder(root->right);
+//
+//}
+//静态
+int size = 0;
+void TreeSize1(BTNode* root)
 {
-	if(root == NULL)
+	if (root == NULL)
 	{
-		printf("NULL ");
 		return;
 	}
-
-	printf("%c ",root->data);
-	PreOrder(root->left);
-	PreOrder(root->right);
+	else
+	{
+		size++;
+	}
+	TreeSize1(root->left);
+	TreeSize1(root->right);
 }
-//涓簭
-void InfixOrder (BTNode* root)
+//可并发，多线程
+void TreeSize2(BTNode* root,int* psize)
 {
-	if(root == NULL)
+	if (root == NULL)
 	{
-		printf("NULL ");
 		return;
 	}
-	InfixOrder(root->left);
-	printf("%c ",root->data);
-	InfixOrder(root->right);
+	else
+	{
+		(*psize)++;
+	}
+	TreeSize2(root->left,psize);
+	TreeSize2(root->right,psize);
 }
-//鍚庡簭
-void PostOrder(BTNode* root)
+//分治
+int TreeSize3(BTNode* root)
 {
-	if(root == NULL)
-	{
-		printf("NULL ");
-		return;
-	}
-	PostOrder(root->left);
-	PostOrder(root->right);
-	printf("%c ",root->data);
+	return root == NULL ? 0 : TreeSize3(root->left) + TreeSize3(root->right) + 1;
 }
 int main()
 {
@@ -52,36 +63,50 @@ int main()
 	A->data = 'A';
 	A->left = NULL;
 	A->right = NULL;
-	
+
 	BTNode* B = (BTNode*)malloc(sizeof(BTNode));
 	B->data = 'B';
 	B->left = NULL;
 	B->right = NULL;
-	
+
 	BTNode* C = (BTNode*)malloc(sizeof(BTNode));
 	C->data = 'C';
 	C->left = NULL;
 	C->right = NULL;
-	
+
 	BTNode* D = (BTNode*)malloc(sizeof(BTNode));
 	D->data = 'D';
 	D->left = NULL;
 	D->right = NULL;
-	
+
 	BTNode* E = (BTNode*)malloc(sizeof(BTNode));
 	E->data = 'E';
 	E->left = NULL;
 	E->right = NULL;
-	
+
 	A->left = B;
 	A->right = C;
 	B->left = D;
 	B->right = E;
-	PreOrder(A);
-	printf("\n");
-	InfixOrder(A);
-	printf("\n");
-	PostOrder(A);
-	printf("\n");
+	//PrevOrder(A);
+	//printf("\n");
+
+	TreeSize1(A);
+	printf("TreeSize1:%d\n", size);
+	size = 0;
+	TreeSize1(B);
+	printf("TreeSize1:%d\n", size);
+
+	int Asize = 0;
+	TreeSize2(A,&Asize);
+	printf("TreeSize2:%d\n", Asize);
+
+	int Bsize = 0;
+	TreeSize2(B,&Bsize);
+	printf("TreeSize2:%d\n", Bsize);
+
+	printf("TreeSize3:%d\n", TreeSize3(A));
+	printf("TreeSize3:%d\n", TreeSize3(B));
+
 	return 0;
 }
